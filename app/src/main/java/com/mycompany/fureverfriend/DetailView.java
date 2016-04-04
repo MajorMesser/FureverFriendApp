9a06a5fb6a035aaa84309b20ab9c3e9d9b54c2a5
Jordan P. Day <jday27@mymts.net>
2016-04-04 11:40:17 -0500

    API call included and async task executing

--- app/src/main/java/com/mycompany/fureverfriend/DetailView.java
+++ app/src/main/java/com/mycompany/fureverfriend/DetailView.java
@@ -1,13 +1,107 @@
 package com.mycompany.fureverfriend;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Xml;
+import android.widget.Toast;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class DetailView extends AppCompatActivity {
+
+    private String api_key = "c2afcd54cfa21c24c13d6f7e57c64e69";
+    private String query;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
+
+        Intent i = getIntent();
+
+        String query = "http://api.petfinder.com/pet.find?key=" + api_key + "&id=" + i.getStringExtra("id");
+
+        new RetrievePetList(query).execute();
+    }
+
+    class RetrievePetList extends AsyncTask<Void, Void, Void> {
+
+        String query;
+
+        public RetrievePetList(String query) {
+            this.query = query;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(DetailView.this, "Searching for pets...", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Toast.makeText(DetailView.this, "Complete!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+                URL url = new URL(this.query);
+                InputStream in = url.openConnection().getInputStream();
+                XmlPullParser parser = Xml.newPullParser();
+                parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+                parser.setInput(in, "UTF_8");
+
+                int eventType = parser.getEventType();
+                boolean inPet = false;
+
+                String id = "";
+                String name = "";
+                String breed = "";
+                String type = "";
+                String gender = "";
+                String location = "";
+                String age = "";
+
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    if (eventType == XmlPullParser.START_TAG) {
+                        if (parser.getName().equalsIgnoreCase("pet")) {
+                            inPet = true;
+                        } else if (inPet && parser.getName().equalsIgnoreCase("id")) {
+                            id = parser.nextText();
+                        } else if (inPet && parser.getName().equalsIgnoreCase("name")) {
+                            name = parser.nextText();
+                        } else if (inPet && parser.getName().equalsIgnoreCase("breed")) {
+                            breed = parser.nextText();
+                        } else if (inPet && parser.getName().equalsIgnoreCase("animal")) {
+                            type = parser.nextText();
+                        } else if (inPet && parser.getName().equalsIgnoreCase("sex")) {
+                            gender = parser.nextText();
+                        } else if (inPet && parser.getName().equalsIgnoreCase("location")) {
+                            location = parser.nextText();
+                        } else if (inPet && parser.getName().equalsIgnoreCase("age")) {
+                            age = parser.nextText();
+                        }
+                    } else if (eventType == XmlPullParser.END_TAG && parser.getName().equalsIgnoreCase("pet")) {
+                        inPet = false;
+                    }
+                    eventType = parser.next();
+                }
+
+            } catch (IOException | XmlPullParserException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
