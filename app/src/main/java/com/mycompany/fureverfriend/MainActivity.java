7c56fd9d20e70e5c46bee82c274d27c0dfd5fa3d
Jordan P. Day <jday27@mymts.net>
2016-04-18 08:14:39 -0500

    shared preferences added to the location and breed

--- app/src/main/java/com/mycompany/fureverfriend/MainActivity.java
+++ app/src/main/java/com/mycompany/fureverfriend/MainActivity.java
@@ -1,8 +1,11 @@
 package com.mycompany.fureverfriend;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     EditText edtLocation;
     EditText edtBreed;
 
+    SharedPreferences pref;
+    SharedPreferences.Editor edit;
 
     LinearLayout linGenderLayout;
     LinearLayout linAgeLayout;
@@ -25,6 +30,60 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pref = getSharedPreferences("main", MODE_PRIVATE);
+        edit = pref.edit();
+
+        edtLocation = (EditText) findViewById(R.id.edtLocation);
+        edtBreed = (EditText) findViewById(R.id.edtBreed);
+
+        edtLocation.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                edit.putString("location", edtLocation.getText().toString());
+                edit.apply();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        edtBreed.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                edit.putString("breed", edtBreed.getText().toString());
+                edit.apply();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        pref = getSharedPreferences("main", MODE_PRIVATE);
+        edtBreed = (EditText) findViewById(R.id.edtBreed);
+        edtLocation = (EditText) findViewById(R.id.edtLocation);
+
+        edtBreed.setText(pref.getString("breed", ""));
+        edtLocation.setText(pref.getString("location", ""));
     }
 
     public void passRequest(View view) {
